79cc6b4d3a1dc8d0b3e1376a6c7a67c5a1814be0
himanshuyadav4211 <83404487+himanshuyadav4211@users.noreply.github.com>
2021-10-08 18:19:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-exec(open('venv/bin/activate_this.py').read(), {'__file__': 'venv/bin/activate_this.py'})
+exec(open('venv/bin/activate_this.py').read(), {'_file_': 'venv/bin/activate_this.py'})
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     print('\033c')
     print('\n\nBeasties')
     print('------------------------\n\n')
@@ -40,55 +40,35 @@ if __name__ == '__main__':
     print('\033c')
     print('\n\nBeasties')
     print('------------------------\n\n')
-    animal = input('What pet do you own ? (Dog/Cat)\n').lower()
+    animal = input('What pet do you own ? (Dog/Cat/others)\n').lower()
     if animal == 'dog':
         print('\033c')
         print('\n\nBeasties')
         print('------------------------\n\n')
 
         breed = input('What is the breed of your dog ? (type \'na\' if you don\'t know)\n').lower()
-        Break = True
     elif animal == 'cat':
         print('\033c')
         print('\n\nBeasties')
         print('------------------------\n\n')
         
         breed = input('Which cat do you have ? (type \'na\' if you don\'t know)\n').lower()
-        Break = True
     else:
         print('\033c')
         print('\n\nBeasties')
         print('------------------------\n\n')
 
-        print('Sorry we do not provide services for them :\'(\n')
+        breed = input('What is the breed/variety of your' + animal + '?\n').lower()
 
     Break = False
     while not Break:
         service_type = int(input('Enter your preferred service:\n1. Basic Wash\n2. Haircut\n3. Wash + massage\n4. Disease/injury treatment\n5. Pet training\n   (enter the number)\n   '))
-        if service_type == 1:
+        if service_type >= 1 and service_type <= 5:
             print('\033c')
             print('\n\nBeasties')
             print('------------------------\n\n')
-            Break = True
-        elif service_type == 2:
-            print('\033c')
-            print('\n\nBeasties')
-            print('------------------------\n\n')
-            Break = True
-        elif service_type == 3:
-            print('\033c')
-            print('\n\nBeasties')
-            print('------------------------\n\n')
-            Break = True
-        elif service_type == 4:
-            print('\033c')
-            print('\n\nBeasties')
-            print('------------------------\n\n')
-            Break = True
-        elif service_type == 5:
-            print('\033c')
-            print('\n\nBeasties')
-            print('------------------------\n\n')
+            
+            print('Thank you ! We shall soon provide the services...')
             Break = True
         else:
             print('\033c')
@@ -96,9 +76,6 @@ if __name__ == '__main__':
             print('------------------------\n\n')
             print('Invalid ! Try again !\n')
 
-    print('\033c')
-    print('\n\nBeasties')
-    print('------------------------\n\n')
     # pet clinic store address and schedules
     
     # petcare worker emails and phone number
